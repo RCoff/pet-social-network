37d672af98182169a2a7e48996f6af4652ce3d32
RCoff <enfaduraa@gmail.com>
2021-06-27 21:25:57 -0400

    Change project name

--- PetSocialNetwork/asgi.py
+++ PetSocialNetwork/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TestDRFSite project.
+ASGI config for PetSocialNetwork project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestDRFSite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PetSocialNetwork.settings')
 
 application = get_asgi_application()
--- PetSocialNetwork/wsgi.py
+++ PetSocialNetwork/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TestDRFSite project.
+WSGI config for PetSocialNetwork project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestDRFSite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PetSocialNetwork.settings')
 
 application = get_wsgi_application()
